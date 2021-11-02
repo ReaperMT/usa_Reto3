399fee1197f22264d96d415d8387b5830fb36fc3
Diego Lopez <diego.lopez.mt@correo.usa.edu.co>
2021-11-01 20:38:13 -0500

    The functionality for category is created

---
+++ src/main/java/com/usa/ciclo3/reto/crudrepository/CategoryCrudRepository.java
@@ -0,0 +1,7 @@
+package com.usa.ciclo3.reto.crudrepository;
+
+import com.usa.ciclo3.reto.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryCrudRepository extends CrudRepository<Category, Integer> {
+}
